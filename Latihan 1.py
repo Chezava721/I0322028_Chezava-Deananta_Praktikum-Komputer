fc732a6e061d4fe24ddd257b3b9456ab7f909748
Chezava721 <chezava.deananta@student.uns.ac.id>
2023-03-13 17:54:02 +0700

    Exercise 1

--- Latihan 1.py
+++ Latihan 1.py
@@ -1,2 +1,14 @@
-print
-print ("praktikuum")
+print("Daftar Gaji")
+nama =  input("Masukkan Nama")
+keljamkerja = int(input("Masukkan Kelebihan Jam Kerja"))
+golongan = int(input("Masukkan Golongan"))
+
+gajipokok = golongan * 1000000
+lembur = keljamkerja * 200000
+pendapatan = gajipokok + lembur
+
+print("Nama = ", nama)
+print("Golongan = ", golongan)
+print("Gaji Pokok = ", gajipokok)
+print("Lembur = ", lembur)
+print("Pendapatan = ", pendapatan)
---
+++ Latihan.py
@@ -0,0 +1,40 @@
+kelas = ('Ali','Budi','Caca','dinda','elo')
+print(kelas[1])
+print(kelas[2])
+
+
+print('============================')
+
+#deklarasi data bunga
+bunga=['melati','mawar','anggrek bulam','anggrek panda']
+print(bunga[3])
+print(len(bunga[3]))
+
+print('==============================')
+#matriks dengan ukuran 3 x 3
+
+#print
+matriksA = [[1,2,3],
+            [4,5,6],
+            [7,8,9]]
+
+matriksB = [[9,8,7],
+            [6,5,4],
+            [3,2,1]]
+
+Hasiljumlah[[0,0,0],
+            [0,0,0],
+            [0,0,0]]
+
+#proses 
+
+#proses penjumlahan matriks 1 dan matriks 2
+
+for x in range(0,len(matriksA))
+    #print()
+    for y in range(0,len(matriksA[0])):
+        hasiljumlah[x][y] = matriksA[x][y]+matriksB[x][y]
+print('==============================')
+
+
+
