ba335686d594679464e01c5fcb17cf7fa812ccb9
Chezava721 <chezava.deananta@student.uns.ac.id>
2023-07-04 20:02:16 +0700

    commit lagi

--- A_Tugas Week 1.py
+++ A_Tugas Week 1.py
@@ -1,23 +1,29 @@
 #Exercise-01
 
 print("="*55) 
-print ("\n"+"Nama   : Akila Syahra Maharani")
-print ("\n"+"Nim    : I0322009")
+print ("\n"+"Nama   : Chezava Deananta")
+print ("\n"+"Nim    : I0322028")
 print("="*55) 
 print ("""
-    Halo, perkenalkan namaku Akila Syahra Maharani, 
-biasa dipanggil Akila. Aku berasal dari Daerah Istimewa
-Yogyakarta atau yang biasa disebut Kota Jogja. Kota Jogja 
-merupakan kota kelahiranku sekaligus tempat dimana aku 
-tumbuh. Kota jogja adalah salah satu kota yang masih 
-memegang erat tradisi jawa yang kuat. Selain memiliki 
-berbagai budaya yang beragam, kuliner yang ada di kota 
-jogja juga cukup khas dan populer sehingga menarik wisatawan
-untuk berkunjung. Sebagai wilayah dengan identitas budaya
-yang kental, menjadikan kota jogja memiliki predikat sebagai
-kota budaya. 
-    Harapanku sebagai seorang mahasiswa teknik industri setelah
-lulus adalah untuk dapat memberikan hal-hal positif di lingkungan 
-serta dapat memanfaatkan ilmu yang telah aku pelajari 
-selama berkuliah di teknik industri UNS.""")
+    Perkenalkan nama saya Chezava Deananta NIM I0322028 . Saya lahir di Kota Solo, 
+Jawa Tengah. Sebelum saya masuk di Teknik Industri UNS ini saya menempuh pendidikan 
+SMA di SMA N 1 Karanganyar.Saya juga lulusan SMP N 1 Karanganyar tercinta dimana saya 
+bertemu sahabat-sahabat sejati saya yang selalu hadir disetiap saya susah dan senang. 
+Kembali lagi ke perkenalan diri saya, Saya memiliki hobi jalan-jalan, traveling, dan 
+fotografi. Saya selalu mengabadikan setiap moment karena waktu tidak berputar dua kali 
+jadi saya harus mengabadikan setiap moment berharga. 
+    Sejak saya dilahirkan oleh ibu saya, saya tinggal di Karanganyar, Jawa Tengah. Entah 
+mengapa kedua orang tua saya memilih Karanganyar untuk dijadikan tempat untuk dijadikan 
+rumah tinggal kita Saya tinggal di perumahan yang letaknya cukup strategis dekat dengan 
+pusat keramaian karanganyar. Lingkungan perumahan saya sangat nyaman, lunayan bersih dan 
+lumayan damai. Lingkungan rumah saya tidak terlalu panas hawanya tidak seperti daerah2 
+perkotaan karena masih banyak dikelilingi sawah dan perkebunan sehingga cenderung sejuk.
+    Harapan saya kedepannya setelah lulus dari Teknik Industri adalah setelah lulus nanti 
+saya bisa lebih banyak memiliki wawasan, ilmu, pengetahuan serta pengalaman-pengalaman baru 
+dari semua yang sudah saya lewati selama empat tahun kuliah di Teknik Indistri UNS ini, 
+sehingga dengan semua hal itu bisa saya manfaatkan saat di dunia kerja nanti dengan harapan 
+bisa mempermudah saya dalam mendapatkan pekerjaan nanti. Cita-cita saya adalah bisa diterima 
+di pekerjaan yang sama dengan kakak dan ayah saya yaitu di dunia pertambangan. walaupun kita 
+tau resiko bekerja di dunia tambang sangat besar, tetapi saya sangat ingin masuk di 
+PT.Freeport Indonesiaa""")
 print("="*55) 
